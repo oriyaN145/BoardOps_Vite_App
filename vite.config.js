f53df9a549dfdfd6d985437ebc04bd562d95ac80
oriyaN145 <135110870+oriyaN145@users.noreply.github.com>
2025-10-26 05:56:40 +0000

    asdasdadasd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Board_Vite_App/', // must match the repo name exactly
+  plugins: [react()]
 })
